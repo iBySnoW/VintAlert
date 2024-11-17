0d8fc1f8796591b00beb1c93f5ce4b63fbe4a19f
legalito <63124952+legalito@users.noreply.github.com>
2024-11-18 00:00:02 +0100

    up

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Get, Post, Param, Body, Put, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get,Query, Post, Param, Body, Put, Delete, ParseIntPipe } from '@nestjs/common';
 import { Public } from '../auth/AuthMetadata'; // Assurez-vous d'avoir ce décorateur
-
+import { MoviesService } from './movies.service';
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService){}
+    @Public()
+    @Get()
+    async GetMovies(){
+        return this.moviesService.getCurrentMovies();
+    }
+
+    @Public()
+    @Post('/:id')
+    async getMovieByID(@Param('id') id: number){
+        return this.moviesService.addMovieById(id);
+    }
+
+    @Public()
+    @Get('/:id')
+    async getMovieByIdFirebase(@Param('id') id: number){
+        return this.moviesService.getMoviesID(id);
+    }
 }
